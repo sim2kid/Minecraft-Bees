74608b800a0ee3c97f5e0980ab1952de39be299c
sim2kid <sim2kid@users.noreply.github.com>
2020-12-01 20:48:16 -0600

    Adds a Beehive with all it's possible textures

---
+++ Assets/Scripts/HiveTextures.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiveTextures : MonoBehaviour
+{
+    [SerializeField] private Texture2D[] frontHiveTextures;
+    [SerializeField] private Material frontHiveMaterial;
+    public int State;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        State = 0;
+        EmptyHive();
+    }
+
+    private void updateHiveTexture() 
+    {
+        frontHiveMaterial.mainTexture = frontHiveTextures[State];
+    }
+
+    public void FillHive() 
+    {
+        if (State < frontHiveTextures.Length-1)
+        {
+            State++;
+            updateHiveTexture();
+        }
+    }
+
+    public void EmptyHive() 
+    {
+        State = 0;
+    }
+}
